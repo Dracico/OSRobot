871fdce6e2aaa71a9ecfa695feddfbfd09a4c40e
efrenbg1 <efren@boyarizo.es>
2023-12-14 14:47:59 +0100

    Included tasks to run compile and run code in the robot

--- robot.c
+++ robot.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
-#include "ev3.h"
-#include "ev3_light.h"
-int main( void )
+
+int main(int argc, const char *argv[])
 {
-    printf( "Hello, LEGO World!\n" );
-    ev3_init();
-    switch ( get_light( LIT_LEFT )) {
-    case LIT_GREEN:
-        set_light( LIT_LEFT, LIT_RED );
-        break;
-    case LIT_RED:
-        set_light( LIT_LEFT, LIT_AMBER );
-        break;
-    default:
-        set_light( LIT_LEFT, LIT_GREEN );
-        break;
-    }
-    ev3_uninit();
+    printf("OS is fantastic!!\n");
+
     return 0;
 }
