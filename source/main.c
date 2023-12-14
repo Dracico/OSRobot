1dcaf16590034ce7fa0d7a5115b8e82ed0728706
efrenbg1 <efren@boyarizo.es>
2023-12-14 15:22:29 +0100

    Made skeleton of project (with Makefile)

---
+++ source/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../libraries/sensors.h"
+
+int main(int argc, const char *argv[])
+{
+    printf("OS is chicken fantastic!!\n");
+    say_hello();
+    return 0;
+}
---
+++ source/sensors.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../libraries/sensors.h"
+
+void say_hello(void)
+{
+    printf("Hello from sensors!\n");
+}
